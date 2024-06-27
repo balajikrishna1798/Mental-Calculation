cc402415b559c6d190f6a7d2e58c4fe1622a3366
Balajikrishna <82527215+balajikrishna1798@users.noreply.github.com>
2024-06-27 15:51:38 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Update the instance and install Node.js, npm, and Git
 yum update -y
-curl -sL https://rpm.nodesource.com/setup_14.x | bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | bash -
 yum install -y nodejs git
