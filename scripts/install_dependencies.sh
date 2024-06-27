d02772ea90ad7d1883271359c893b21bb4cd1d00
Balajikrishna <82527215+balajikrishna1798@users.noreply.github.com>
2024-06-27 16:18:15 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,10 +3,10 @@
 cd /home/ec2-user/nextjs-app
 
 # Install project dependencies
-npm install
+sudo npm install
 
 # Build the Next.js application
-npm run build
+sudo npm run build
 
 # Verify the build directory is created
 if [ ! -d "build" ]; then
