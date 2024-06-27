2b1cd9beafeebbd52184518ca3ee10b8efd50591
Balajikrishna <82527215+balajikrishna1798@users.noreply.github.com>
2024-06-27 17:42:27 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Navigate to the project directory
-cd /home/ec2-user/nextjs-app
+cd /home/ec2-user/your-nextjs-app
 
-# Install project dependencies
-sudo npm install
+# Install project dependencies and log output
+npm install > /home/ec2-user/your-nextjs-app/npm-install.log 2>&1
 
-# Build the Next.js application
-sudo npm run build
+# Build the Next.js application and log output
+npm run build > /home/ec2-user/your-nextjs-app/npm-build.log 2>&1
 
 # Verify the build directory is created
-if [ ! -d "build" ]; then
+if [ ! -d ".next" ]; then
     echo "Build directory not found. Build failed."
+    cat /home/ec2-user/your-nextjs-app/npm-build.log
     exit 1
 fi
